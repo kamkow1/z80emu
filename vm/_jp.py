00a50f1df657681c587f12cf7a98d39a58169d3b
kamkow1 <ikamileq@gmail.com>
2023-04-29 20:59:06 +0200

    Fix formatting in vm/_jp.py

--- vm/_jp.py
+++ vm/_jp.py
@@ -4,17 +4,21 @@ def perform_jp(self):
     high_byte = self.ram[self.registers["PC"].value]
     self.registers["PC"].value = (high_byte << 8) | low_byte
 
+
 def jp_if(self, cond):
     if cond:
         perform_jp(self)
     else:
         self.increment_pc()
 
+
 def handler_jp_n_n(self, opcode):
     perform_jp(self)
 
+
 def handler_jp_nz_n_n(self, opcode):
     jp_if(self, not self.registers["Z"].value)
+
 
 def handler_jp_z_n_n(self, opcode):
     jp_if(self, self.registers["Z"].value)
