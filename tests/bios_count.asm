1263a5fee7d10ae2e2e7cdbb7ea52c960154f04c
kamkow1 <ikamileq@gmail.com>
2023-05-06 22:37:47 +0200

    Implement `SUB r8`

--- tests/bios_count.asm
+++ tests/bios_count.asm
@@ -3,6 +3,7 @@
   .lf out.lst
   .sf out.sym
   .in bios/bios_defs.inc
+  in tests/conversions.inc
 
 
 main:
@@ -29,48 +30,4 @@ loop:
 
 end:
     halt
-
-num_to_hl_point:
-  ld a, (hl)
-  
-num_to_a:
-  ld h, 0
-  ld l, a
-
-num_to_hl:
-  ld bc, -10000
-  call one
-  ld bc, -1000
-  call one
-  ld bc, -100
-  call one
-  ld bc, -10
-  call one
-  ld c, -1
-one:
-  ld a, '0'-1
-two:
-  inc a
-  add hl, bc
-  jr c, two
-  push bc
-  push af
-  ld a, b
-  cpl
-  ld b, a
-  ld a, c
-  cpl
-  ld c, a
-  inc bc
-  call c, carry
-  pop af
-  add hl, bc
-  pop bc
-  ld (de), a
-  inc de
-  ret
-
-carry:
-  dec bc
-  ret
 
