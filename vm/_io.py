0febe7e3346a743e48ec4350b419b2b6c260693b
kamkow1 <ikamileq@gmail.com>
2023-05-24 14:16:05 +0200

    Document vm/_io.py

--- vm/_io.py
+++ vm/_io.py
@@ -1,3 +1,6 @@
+# handler for `OUT (n), A`
+# takes the value of A reg and outputs
+# it to a port specififed by (n)
 def handler_out_n_a(self, opcode):
     port = self.ram[self.registers["PC"].value]
     self.increment_pc()
@@ -6,6 +9,9 @@ def handler_out_n_a(self, opcode):
     self.vm_lock.release()
 
 
+# handler for `IN a, (n)`
+# takes the value passed onto a port
+# specified by (n) and puts it into the A register
 def handler_in_a_n(self, opcode):
     port = self.ram[self.registers["PC"].value]
     self.increment_pc()
