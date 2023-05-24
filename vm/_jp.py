d32c3add2714c05bdb1440fb7590e0c37f51726b
kamkow1 <ikamileq@gmail.com>
2023-05-24 14:24:40 +0200

    Document vm/_jp.py

--- vm/_jp.py
+++ vm/_jp.py
@@ -1,3 +1,7 @@
+# helper function for performing `JP ...` insutrctions
+# arg value is loaded up from ram as the next byte
+# the `PC` reg is set to the arg, making the VM
+# change it's execution point
 def perform_jp(self):
     low_byte = self.ram[self.registers["PC"].value]
     self.increment_pc()
@@ -5,6 +9,7 @@ def perform_jp(self):
     self.registers["PC"].value = (high_byte << 8) | low_byte
 
 
+# wrapper for conditional jumps
 def jp_if(self, cond):
     if cond:
         perform_jp(self)
@@ -13,13 +18,18 @@ def jp_if(self, cond):
         self.increment_pc()
 
 
+# handler for `JP nn`
 def handler_jp_n_n(self, opcode):
     perform_jp(self)
 
 
+# handler for `JP nz, nn`
+# jumps if the `Z` flag is unset
 def handler_jp_nz_n_n(self, opcode):
     jp_if(self, not self.flags["Z"].value)
 
 
+# handler for `JP z, nn`
+# jumps if the `Z` flag is set
 def handler_jp_z_n_n(self, opcode):
     jp_if(self, self.flags["Z"].value)
